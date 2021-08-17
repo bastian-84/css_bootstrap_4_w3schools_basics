4c670f91246168fffb6885a481561c0fa62bf29c
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-08-16 20:17:50 -0700

    SR: next: BS4 Utilities

--- bootstrap w3schools/js/script.js
+++ bootstrap w3schools/js/script.js
@@ -4,3 +4,34 @@ function el(id){
 };
 // ____________________________________
 
+console.log('test');
+
+// For https://www.w3schools.com/bootstrap4/tryit.asp?filename=trybs_form_validation_needs&stacked=h
+// Disable form submissions if there are invalid fields
+/* 
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Get the forms we want to add validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+ */
+
+
+// For https://www.w3schools.com/bootstrap4/tryit.asp?filename=trybs_form_custom_file&stacked=h
+  // Add the following code if you want the name of the file appear on select
+$(".custom-file-input").on("change", function() {
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+  });
